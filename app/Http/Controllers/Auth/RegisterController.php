d9de1b36e18026a9191a7608f0ac61400c01f5b0
fadilaahmads <fadilahmad59@gmail.com>
2020-06-22 14:08:40 +0700

    Initial Commit

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $user = 1;
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'is_admin' => $data[$user]
         ]);
     }
 }
