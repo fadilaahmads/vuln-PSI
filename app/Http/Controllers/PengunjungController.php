1d6ee45437768b07f9bfbc350d653de69c0dcd93
rynaldysh <shulton.ghiffary23@gmail.com>
2020-06-26 23:30:51 +0700

    pengunjung

--- app/Http/Controllers/PengunjungController.php
+++ app/Http/Controllers/PengunjungController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\facades\DB;
 class PengunjungController extends Controller
 {
-    //
+    public function index()
+    {
+        $pengunjung = DB::table('pengunjung')->get();
+        return view('pengunjung', ['pengunjung' => $pengunjung]);
+    }
 }
